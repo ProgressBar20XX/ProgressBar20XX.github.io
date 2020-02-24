488ecc463adc36ce5f41f775b52a31a60bb771f0
Weilei Zeng <240155787@qq.com>
2020-02-23 16:46:27 -0800

    add email content

---
+++ email/create_content.sh
@@ -0,0 +1,10 @@
+echo Dear XXX, > content.html
+echo >> content.html
+echo Welcome to join my email list. >>content.html
+echo >> content.html
+echo "check out the parameter: $0 , $1" >>content.html
+
+echo >> content.html
+echo from >> content.html
+echo >> content.html
+echo ProgressBar20XX >> content.html
