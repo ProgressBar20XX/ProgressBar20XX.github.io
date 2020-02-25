817b08826d7471dce6716ae63574363f7e9a2cf5
Radish Meeting <240155787@qq.com>
2020-02-24 18:20:22 -0800

    Update counting_down_2020.sh

--- email/counting_down_2020.sh
+++ email/counting_down_2020.sh
@@ -1,5 +1,7 @@
+# this script calculate the time and print the content of the email into counting_down-2020.html
 
-# clculate the time diff
+
+# calculate the time duration
 
 dt=`python -c "from datetime import date as d; print (d(2020, 12, 31) - d.today() ).days"`
 
@@ -10,13 +12,6 @@ dt_text="there are $dt days left before 2020 ends"
 
 outfile=counting_down_2020.html
 
-# this is my other test
-receiver="Mr./Mrs."
-sender="Radish Meeting"
-text=`cat template.html`
-echo $text >> content.html
-
-
 text="Dear $receiver,<br> 
 <h4> ${dt_text} </h4> <br>
 Welcome to join my email list.<br> 
@@ -24,6 +19,7 @@ Hope you enjoy it!<br>
 from<br> ProgressBar20XX ( $sender ) <br> 
 $( date )
 "
+# clear the file first and then add the text
 echo > $outfile
 echo $text >> $outfile
 
